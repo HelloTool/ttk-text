8eca10aff43570c0790078c76f933f29ae7f9d2e
Jesse205 <2055675594@qq.com>
2025-02-19 08:40:06 +0800

    perf(example): 优化 DPI 设置和布局间距
    - 在 Windows 平台上设置 DPI 感知，提高应用显示效果
    - 调整窗口内控件的间距
    - 设置 Entry、ThemedText 和 ScrolledText 的宽度和高度，优化显示效果

--- example.py
+++ example.py
@@ -1,12 +1,18 @@
+import sys
 from tkinter import StringVar, Tk
 from tkinter.ttk import Entry, OptionMenu, Style
 
 from ttk_text import ThemedText
 from ttk_text.scrolled_text import ScrolledText
 
+if sys.platform == "win32":
+    from ctypes import windll
+
+    windll.user32.SetProcessDPIAware()
+
 root = Tk()
-root.geometry("300x300")
 root.title("ThemedText")
+root.configure(padx="4p", pady="4p")
 style = Style(root)
 
 # Update background color when theme changes
@@ -14,18 +20,18 @@ root.bind("<<ThemeChanged>>", lambda _: root.configure(background=style.lookup("
 
 # noinspection PyTypeChecker
 theme_menu = OptionMenu(root, StringVar(root), style.theme_use(), *style.theme_names(), command=style.theme_use)
-theme_menu.pack(padx="7p", pady="7p")
+theme_menu.pack(padx="4p", pady="4p")
 
-entry = Entry(root)
+entry = Entry(root, width=40)
 entry.insert(1, "Hello, Entry!")
-entry.pack(fill="x", padx="7p", pady=(0, "7p"))
+entry.pack(fill="x", padx="4p", pady="4p")
 
-themed_text = ThemedText(root)
-themed_text.pack(fill="both", expand=True, padx="7p", pady=(0, "7p"))
+themed_text = ThemedText(root, width=40, height=10)
+themed_text.pack(fill="both", expand=True, padx="4p", pady="4p")
 themed_text.insert("1.0", "Hello, ThemedText!\n" * 50)
 
-scrolled_text = ScrolledText(root)
-scrolled_text.pack(fill="both", expand=True, padx="7p", pady=(0, "7p"))
+scrolled_text = ScrolledText(root, width=40, height=10)
+scrolled_text.pack(fill="both", expand=True, padx="4p", pady="4p")
 scrolled_text.insert("1.0", "Hello, ScrolledText!\n" * 50)
 
 root.mainloop()
