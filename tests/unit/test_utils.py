032d8e415d0312684145525313d4728f67b59ce2
Jesse205 <2055675594@qq.com>
2025-09-30 12:43:03 +0800

    feat(tests): 添加解析填充的单元测试

---
+++ tests/unit/test_utils.py
@@ -0,0 +1,52 @@
+# tests/test_utils.py
+import pytest
+from ttk_text.utils import parse_padding
+
+
+class TestParsePadding:
+    def test_parse_tuple_left(self):
+        result = parse_padding(5)
+        assert result is not None
+        assert result.to_padx() == (5, 5)
+        assert result.to_pady() == (5, 5)
+
+    def test_parse_tuple_left_top(self):
+        result = parse_padding((5, 10))
+        assert result is not None
+        assert result.to_padx() == (5, 5)
+        assert result.to_pady() == (10, 10)
+
+    def test_parse_tuple_left_top_right_bottom(self):
+        result = parse_padding((1, 2, 3, 4))
+        assert result is not None
+        assert result.to_padx() == (1, 3)
+        assert result.to_pady() == (2, 4)
+
+    def test_parse_string_left(self):
+        result5p = parse_padding("5p")
+        assert result5p is not None
+        assert result5p.to_padx() == ("5p", "5p")
+        assert result5p.to_pady() == ("5p", "5p")
+        
+
+    def test_parse_string_left_top(self):
+        result5p10p = parse_padding("5p 10p")
+        assert result5p10p is not None
+        assert result5p10p.to_padx() == ("5p", "5p")
+        assert result5p10p.to_pady() == ("10p", "10p")
+
+    def test_parse_string_left_top_right_bottom(self):
+        result5p10p15p20p = parse_padding("5p 10p 15p 20p")
+        assert result5p10p15p20p is not None
+        assert result5p10p15p20p.to_padx() == ("5p", "15p")
+        assert result5p10p15p20p.to_pady() == ("10p", "20p")
+
+
+    def test_parse_none(self):
+        result = parse_padding(None)
+        assert result is None
+
+
+    # def test_parse_invalid(self):
+    #     with pytest.raises(ValueError):
+    #         parse_padding("invalid")
